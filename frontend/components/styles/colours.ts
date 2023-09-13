9fbcee72edf5da4964c31637c60dc12bbe136398
danriley11 <dan_riley7@outlook.com>
2023-09-13 18:10:20 +1000

    update styles folder

---
+++ frontend/components/styles/colours.ts
@@ -0,0 +1,15 @@
+export const softGrey = 'RGBA(0, 0, 0, 0.05)';
+export const softGreySolid = '#F9F9F9';
+export const lightGrey = 'RGBA(150, 150, 150, 0.5)';
+export const lightGreySolid = '#969696';
+export const darkGrey = `RGBA(0, 0, 0, 0.8)`;
+export const darkGreySolid = '#323232';
+
+export const white = 'RGBA(255, 255, 255, 1)';
+export const offWhite = 'RGBA(245, 245, 245, 1)';
+export const black = 'RGBA(0, 0, 0, 1)';
+
+export const lightCoral = 'RGBA(240, 128, 128, 0.5)';
+export const lightCoralSolid = 'RGBA(240, 128, 128)';
+export const darkCandy = 'RGBA(164, 38, 38, 0.8)';
+export const darkCandySolid = 'RGBA(164, 38, 38)';
