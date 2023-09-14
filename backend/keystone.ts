f390a73c9b02710acdf9921132f1e5b87010740f
danriley11 <dan_riley7@outlook.com>
2023-09-14 15:44:21 +1000

    update Keystone with User access and User UI

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
+import { createAuth } from '@keystone-next/auth';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-bespoke-designs';
 
@@ -9,25 +12,40 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add initial roles here -> will become admin
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // add data seeding here
-  },
-  lists: createSchema({
-    // schema items here
-  }),
-  // Do we want people to have access to keystoneUI
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  //   TODO: add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // add data seeding here
+    },
+    lists: createSchema({
+      // schema items here
+      User,
+    }),
+    // Do we want people to have access to keystoneUI
+    ui: {
+      //   Only show UI to those who pass this test:
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  }),
+);
