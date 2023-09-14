d84395f6a617016fe71d2d60bc03265a6c6fa6b0
danriley11 <dan_riley7@outlook.com>
2023-09-14 18:06:43 +1000

    resolve undefined CLOUDINARY_CLOUD_NAME value

--- backend/schemas/ProductImage.ts
+++ backend/schemas/ProductImage.ts
@@ -1,10 +1,13 @@
+import 'dotenv/config';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
 import { list } from '@keystone-next/keystone/schema';
 import { text } from '@keystone-next/fields';
-import 'dotenv/config';
+
+// console.log('============================');
+// console.log('cloudinary', cloudinary);
 
 export const cloudinary = {
-  cloudName: process.env.CLOUDINARY_NAME,
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_KEY,
   apiSecret: process.env.CLOUDINARY_SECRET,
   folder: 'bespokedesigns',
