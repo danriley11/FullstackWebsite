3721fad868cda8f0662fe73089c4fa4410b2cf89
danriley11 <dan_riley7@outlook.com>
2023-09-15 13:43:36 +1000

    Calling _app via apollo fn using Next.js features

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,8 @@ import Page from '../components/Page';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import '../components/givenStyles/nprogress.css';
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -12,12 +14,24 @@ type MyAppProps = {
   Component: ReactNode;
   pageProps: any;
 };
-const MyApp = ({ Component, pageProps }: MyAppProps) => {
+const MyApp = ({ Component, pageProps, apollo }: MyAppProps) => {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 };
 
-export default MyApp;
+// Tell Next.js to fetch all the [Apollo] queries in the child component
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
