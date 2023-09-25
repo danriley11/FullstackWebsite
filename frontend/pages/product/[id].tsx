c31b0b503bb5e6533f05f79552608e7c6e9021cf
danriley11 <dan_riley7@outlook.com>
2023-09-25 12:39:15 +1000

    create template to dynamically render each product

---
+++ frontend/pages/product/[id].tsx
@@ -0,0 +1,44 @@
+// This filename convention is specific to Next.JS when rendering based on the url
+
+import { useQuery } from '@apollo/client';
+import SINGLE_ITEM_QUERY from '../../components/SingleItem/SingleItem.graphql';
+import DisplayError from '../../components/ErrorMessage';
+import Head from 'next/head';
+import { ProductStyles } from '../../components/SingleItem/SingleItem.styles';
+
+type SingleProductProps = {
+  query: string;
+};
+const SingleProductPage = ({ query }: SingleProductProps) => {
+  const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
+    variables: {
+      id: query.id,
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <DisplayError error={error} />;
+
+  const { Product } = data;
+
+  // Render product-specific content
+  console.log(Product);
+  return (
+    <ProductStyles>
+      {/* Utilise NextJS to update Tab text */}
+      <Head>
+        <title>Bespoke designs | {Product.name}</title>
+      </Head>
+
+      <img src={Product.photo.image.publicUrlTransformed} alt={Product.photo.alt} />
+      <div>
+        <p>{Product.name}</p>
+        <p>{Product.price}</p>
+        <p>{Product.description}</p>
+      </div>
+    </ProductStyles>
+  );
+};
+
+export default SingleProductPage;
