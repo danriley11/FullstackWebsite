d366066aa63605e8cdd85e39c8d408e53a281680
danriley11 <dan_riley7@outlook.com>
2023-09-28 17:45:09 +1000

    Conditionally render NAV items based on user data

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -1,13 +1,27 @@
 import Link from 'next/link';
 import StyledNav from './styles/Nav.styles';
+import useUser from '../utils/useUser';
 
 const Nav = () => {
+  const user = useUser();
+
   return (
     <StyledNav>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
+
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+
+      {!user && (
+        <>
+          <Link href="/signin">Sign in</Link>
+        </>
+      )}
     </StyledNav>
   );
 };
