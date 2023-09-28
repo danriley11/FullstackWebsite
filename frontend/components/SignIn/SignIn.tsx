5c171ca51bff886c0178803b0a02e6896ec04ab4
danriley11 <dan_riley7@outlook.com>
2023-09-28 17:37:08 +1000

    Create SignIn component

---
+++ frontend/components/SignIn/SignIn.tsx
@@ -0,0 +1,83 @@
+import { useEffect } from 'react';
+import useForm from '../../utils/useForm';
+import { CURRENT_USER_QUERY } from '../User/User.graphql';
+import Form from '../styles/Form.styles';
+import { SIGNIN_MUTATION } from './SignIn.graphql';
+import { useMutation } from '@apollo/client';
+import DisplayError from '../ErrorMessage';
+import { useUser } from '../User/User';
+import router from 'next/router';
+
+const SignIn = () => {
+  const user = useUser();
+  const { inputs, handleChange, resetForm } = useForm(
+    {
+      email: '',
+      password: '',
+    },
+    false,
+  );
+
+  const [signin, { data }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+  console.log('signin data', data);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await signin();
+    resetForm();
+  }
+
+  // Redirect user to their account page on successful sign in
+  useEffect(() => {
+    if (user) {
+      router.push({
+        pathname: `/account`,
+      });
+    }
+  }, [user]);
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign into your account</h2>
+
+      <DisplayError error={data?.authenticateUserWithPassword} />
+
+      <fieldset>
+        <label htmlFor="email">
+          Email:{' '}
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="samson@thewise.com"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+          <br />
+        </label>
+
+        <label htmlFor="password">
+          Password:{' '}
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="********"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+          <br />
+        </label>
+
+        <button type="submit">Sign in</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default SignIn;
