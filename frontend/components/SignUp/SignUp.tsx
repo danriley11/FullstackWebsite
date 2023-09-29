a6a6ffc91896d1369f93b702c86ca837c2f6a302
danriley11 <dan_riley7@outlook.com>
2023-09-29 11:45:05 +1000

    Create SignUp component

---
+++ frontend/components/SignUp/SignUp.tsx
@@ -0,0 +1,87 @@
+import useForm from '../../utils/useForm';
+import Form from '../styles/Form.styles';
+import { SIGNUP_MUTATION } from './SignUp.graphql';
+import { useMutation } from '@apollo/client';
+import DisplayError from '../ErrorMessage';
+
+const SignUp = () => {
+  const { inputs, handleChange, resetForm } = useForm(
+    {
+      signUpName: '',
+      signUpEmail: '',
+      signUpPassword: '',
+    },
+    false,
+  );
+
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+    variables: inputs,
+  });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await signup().catch(console.error);
+    console.log('signUp', data, loading, error);
+    resetForm();
+  }
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign up for an account</h2>
+
+      <DisplayError error={error} />
+
+      {data?.createUser && <p>You have successfully signed up with {data.createUser.email}!</p>}
+
+      <fieldset>
+        <label htmlFor="signUpName">
+          Name:{' '}
+          <input
+            type="text"
+            id="signUpName"
+            name="signUpName"
+            placeholder="samson thewise"
+            autoComplete="name"
+            value={inputs.signUpName}
+            onChange={handleChange}
+          />
+          <br />
+        </label>
+
+        <label htmlFor="signUpEmail">
+          Email:{' '}
+          <input
+            type="email"
+            id="signUpEmail"
+            name="signUpEmail"
+            // TODO: prevent autocomplete
+            autoComplete="off"
+            placeholder="samson@thewise.com"
+            value={inputs.signUpEmail}
+            onChange={handleChange}
+          />
+          <br />
+        </label>
+
+        <label htmlFor="signUpPassword">
+          Password:{' '}
+          <input
+            type="password"
+            id="signUpPassword"
+            name="signUpPassword"
+            // TODO: prevent autocomplete
+            autoComplete="off"
+            placeholder="********"
+            value={inputs.signUpPassword}
+            onChange={handleChange}
+          />
+          <br />
+        </label>
+
+        <button type="submit">Sign up</button>
+      </fieldset>
+    </Form>
+  );
+};
+
+export default SignUp;
