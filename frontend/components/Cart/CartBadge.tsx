d0acd39d7ae684be510262313ff95b9eef356222
danriley11 <dan_riley7@outlook.com>
2023-10-04 17:40:07 +1100

    Create CartBadge component

---
+++ frontend/components/Cart/CartBadge.tsx
@@ -0,0 +1,18 @@
+import { AnimationStyles, Badge } from '../styles/Cart.styles';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+const CartBadge = ({ count }) => {
+  return (
+    // Apply animation styles to the duplicated components
+    <AnimationStyles>
+      <TransitionGroup>
+        {/* Duplicate the component */}
+        <CSSTransition unmountOnExit className="count" classNames="count" key={count} timeout={{ enter: 400, exit: 400 }}>
+          <Badge>{count}</Badge>
+        </CSSTransition>
+      </TransitionGroup>
+    </AnimationStyles>
+  );
+};
+
+export default CartBadge;
