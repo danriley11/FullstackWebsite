e88ac76d4cde8c67204efdddf1b68b0b7a0bd026
danriley11 <dan_riley7@outlook.com>
2023-10-04 14:27:01 +1100

    Include Cart Item schema in KeystoneJS

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,7 @@ import { withItemData, statelessSessions } from '@keystone-next/keystone/session
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { CartItem } from './schemas/CartItem';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 
@@ -60,6 +61,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
     // Do we want people to have access to keystoneUI
     ui: {
