a25f6f043ad4ced00db28b7187974dd3163ec0fb
danriley11 <dan_riley7@outlook.com>
2023-10-04 17:04:52 +1100

    adding reference to custom queries

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -9,6 +9,7 @@ import { ProductImage } from './schemas/ProductImage';
 import { CartItem } from './schemas/CartItem';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-bespoke-designs';
 
@@ -63,6 +64,7 @@ export default withAuth(
       ProductImage,
       CartItem,
     }),
+    extendGraphqlSchema,
     // Do we want people to have access to keystoneUI
     ui: {
       // Only show UI to those who pass this test:
