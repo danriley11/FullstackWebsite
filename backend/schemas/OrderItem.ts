a495b849d76331a001af200fbd1ae18aef7e5886
danriley11 <dan_riley7@outlook.com>
2023-10-06 16:35:11 +1100

    Create OrderItem schema

---
+++ backend/schemas/OrderItem.ts
@@ -0,0 +1,31 @@
+import { text, integer, relationship } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
+
+export const OrderItem = list({
+  fields: {
+    name: text({ isRequired: true }),
+    description: text({
+      ui: {
+        displayMode: 'textarea',
+      },
+    }),
+    photo: relationship({
+      // one way relationship
+      ref: 'ProductImage',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
+    price: integer(),
+    quantity: integer(),
+    order: relationship({ ref: 'Order.items' }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['photo', 'name', 'quantity', 'price'],
+    },
+  },
+});
