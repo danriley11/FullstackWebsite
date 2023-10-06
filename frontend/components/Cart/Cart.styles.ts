86d543e54bed9bdbcc3b02899da297a0bce04333
danriley11 <dan_riley7@outlook.com>
2023-10-06 16:36:49 +1100

    cleanup Cart.styles

--- frontend/components/Cart/Cart.styles.ts
+++ frontend/components/Cart/Cart.styles.ts
@@ -31,8 +31,6 @@ const CartStyles = styled.div`
     border-top: 10px double black;
     margin-top: 2rem;
     padding-top: 2rem;
-    display: grid;
-    grid-template-columns: auto auto;
     align-items: center;
     font-size: 3rem;
     font-weight: 900;
@@ -52,7 +50,6 @@ const CartStyles = styled.div`
   button {
     &:hover {
       cursor: pointer;
-      color: red;
     }
 
     &:disabled {
