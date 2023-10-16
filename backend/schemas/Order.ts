6910ea6a21f8b2c1198e04d8f2c2fc5f22e29f4d
danriley11 <dan_riley7@outlook.com>
2023-10-16 15:04:58 +1100

    Update Order access rules

--- backend/schemas/Order.ts
+++ backend/schemas/Order.ts
@@ -1,8 +1,15 @@
 import { text, integer, relationship, virtual } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import formatMoney from '../lib/formatMoney';
+import { isSignedIn, rules } from '../access';
 
 export const Order = list({
+  access: {
+    create: isSignedIn,
+    read: rules.canOrderRule,
+    update: () => false,
+    delete: () => false,
+  },
   fields: {
     // Creating custom fieldname
     orderTotal: virtual({
