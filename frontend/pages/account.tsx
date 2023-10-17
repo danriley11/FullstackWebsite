6635f2a6ed20bf7ddea6af48e61c97c493b9572e
danriley11 <dan_riley7@outlook.com>
2023-10-17 16:35:57 +1100

    Update Account page render

--- frontend/pages/account.tsx
+++ frontend/pages/account.tsx
@@ -1,7 +1,25 @@
+import { useRouter } from 'next/router';
+import useUser from '../utils/useUser';
+
 export default function AccountPage() {
+  const router = useRouter();
+  const me = useUser();
+
+  if (!me) {
+    router.push({
+      pathname: '/signin',
+    });
+  }
+
   return (
     <div>
       <p>Hello world!</p>
+      {/* TODO:
+        Checkbox settings list:
+          - Light mode/Dark mode
+          - Toggle for auto-popout of cart after successfully adding new product
+          - Toggle for auto-redirecting admin to product page after successfully implementing (incase of multiple items to add).
+      */}
     </div>
   );
 }
