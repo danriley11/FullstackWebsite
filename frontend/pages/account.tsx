65f60337b64b55365be1fd87e9c7422aaff39055
danriley11 <dan_riley7@outlook.com>
2023-10-17 18:08:48 +1100

    Add Account TODO

--- frontend/pages/account.tsx
+++ frontend/pages/account.tsx
@@ -11,6 +11,7 @@ export default function AccountPage() {
           - Light mode/Dark mode
           - Toggle for auto-popout of cart after successfully adding new product
           - Toggle for auto-redirecting admin to product page after successfully implementing (incase of multiple items to add).
+          - Define how many products to show per page (var = perPage; config.js)
       */}
     </div>
   );
