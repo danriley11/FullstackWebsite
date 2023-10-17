54d9d02206e46d7ca5eb904e1f888ae6d16d4ce2
danriley11 <dan_riley7@outlook.com>
2023-10-17 17:03:28 +1100

    update pagination component stylings

--- frontend/components/Pagination/Pagination.styles.ts
+++ frontend/components/Pagination/Pagination.styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import { lightCoral } from '../styles/core/colours';
 
 const PaginationStyles = styled.div`
   text-align: center;
   display: inline-grid;
   grid-template-columns: repeat(4, auto);
-  align-items: stretch;
+  align-items: center;
   justify-content: center;
   align-content: center;
   margin: 2rem 0;
@@ -15,8 +16,23 @@ const PaginationStyles = styled.div`
     margin: 0;
     padding: 15px 30px;
     border-right: 1px solid lightGray;
+
     &:last-child {
       border-right: 0;
+
+      &:hover {
+        background: ${lightCoral};
+        border-radius: 0 10px 10px 0;
+        text-decoration: none;
+      }
+    }
+
+    &:first-child {
+      &:hover {
+        background: ${lightCoral};
+        border-radius: 10px 0 0 10px;
+        text-decoration: none;
+      }
     }
   }
 
