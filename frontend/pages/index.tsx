496f5807518fb4c28cbf8068c45c9a0d61c924b6
danriley11 <dan_riley7@outlook.com>
2023-10-17 18:09:04 +1100

    Add content to homepage

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,24 @@
+import Head from 'next/head';
+import { ButtonLink } from '../components/styles/buttons/buttons';
+
 export default function IndexPage() {
   return (
-    <div>
-      <p>Hello world!</p>
-    </div>
+    <>
+      <header>
+        <h1>Welcome to Bespoke Designs by Sharon!</h1>
+      </header>
+
+      <div>
+        <p>This space is the home of all my products and is currently undergoing further development and fine-tuning.</p>
+        <p>Please also note that this site currently only supports desktop and will support tablet and mobile in time.</p>
+        <p>
+          For further information about the development of this website, feel free to check it out at{' '}
+          <ButtonLink target="_blank" href="https://www.dansdevden.com">
+            DansDevDen.com
+          </ButtonLink>{' '}
+          and navigate to the Showcasing suite.
+        </p>
+      </div>
+    </>
   );
 }
